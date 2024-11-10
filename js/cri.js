51550eaa7230a4c9c4f583bdfa4f6b1799de4d4f
guyssar-db <166507389+guyssar-db@users.noreply.github.com>
2024-11-10 15:22:11 +0700

    Delete js directory

--- js/cri.js
+++
@@ -1,5 +0,0 @@
-function crilegend() {
-    var legendcriinputText = document.getElementById('cri_legend').value;
-    document.getElementById('legendcri').innerText = legendcriinputText;
-    document.getElementById('legendcri1').innerText = legendcriinputText;
-}
--- js/def.js
+++
@@ -1,5 +0,0 @@
-function deflegend() {
-    var legenddefinputText = document.getElementById('def_legend').value;
-    document.getElementById('legenddef').innerText = legenddefinputText;
-    document.getElementById('legenddef1').innerText = legenddefinputText;
-}
